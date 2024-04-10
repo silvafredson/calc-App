11fd197e2e28b23cd7f592d8d0177205251ee543
Fredson Silva <silva.fredson@hotmail.com>
2024-04-10 19:42:51 -0300

    Initial Commit

---
+++ calc-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calc-App
+//
+//  Created by Fredson Silva on 10/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calc-App/calc_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  calc_AppApp.swift
+//  calc-App
+//
+//  Created by Fredson Silva on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct calc_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
