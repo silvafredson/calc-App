1b863829c26faf97a759379911306c33b108ecbb
Fredson Silva <silva.fredson@hotmail.com>
2024-04-10 22:37:59 -0300

    First basic layout

--- calc-App/ContentView.swift
+++ calc-App/ContentView.swift
@@ -8,16 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        "7", "8", "9", "/",
+        "4", "5", "6", "x",
+        "1", "2", "3", "-",
+        "0", "=", "C", "+"
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4), spacing: 20) {
+                ForEach(buttons, id: \.self) { button in
+                    Button(action: {
+                        // Ação do botão aqui
+                    }) {
+                        Text(button)
+                            .frame(width: button == "0" ? 80 : 40, height: 40)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
+
+
 
 #Preview {
     ContentView()
